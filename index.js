fdda69b94a6c107ce9a503aa121bd9b0de169b0b
Steve Smodish <smodish@hotmail.com>
2021-09-09 14:01:06 -0400

    first working server

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require('express')
+
+const server = express()
+
+server.get('/', (req, res) => {
+  res.json({ message: 'Hello from the Server' })
+})
+
+server.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}!`))
