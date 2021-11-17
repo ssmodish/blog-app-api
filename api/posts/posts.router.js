f649be69e0c00d0a33e1124aeedfb5a3ac17d28c
Steve Smodish <smodish@hotmail.com>
2021-11-16 23:14:44 -0500

    Merging changes

--- api/posts/posts.router.js
+++ api/posts/posts.router.js
@@ -9,26 +9,30 @@ router.get('/', async (req, res) => {
     const data = await Posts.get()
     res.json(data)
   } catch (error) {
-   res.send(error)
+    res.send(error)
   }
 })
+
 // [READ] a post
 router.get('/:id', async (req, res) => {
   const id = req.params.id
   const data = await Posts.getById(id)
   res.json(data)
 })
+
 // [CREATE] a new post
 router.post('/', async (req, res) => {
   const newPost = await Posts.create(req.body)
   res.status(201).json(newPost)
 })
+
 // [UPDATE] the data in a post
 router.put('/:id', async (req, res) => {
   const id = req.params.id
   const updatedPost = await Posts.update(id, req.body)
   res.json(updatedPost)
 })
+
 // [DELETE] a post
 router.delete('/:id', async (req, res) => {
   const deletedPost = await Posts.remove(req.params.id)
